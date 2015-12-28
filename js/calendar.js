12eca56ad1bde3e61f7a58935a0e70ab70694964
Christine <christinebeaubrun@gmail.com>
2015-12-28 14:00:05 -0500

    Html and css for calendar widget

---
+++ js/calendar.js
@@ -0,0 +1,5 @@
+function Calendar (argument) {
+	var wrap,
+		labels,
+		months = ['January', 'Feburary', 'March', 'April', 'May','June','July','August','September','October','November','December'];
+}
